08255fc952dbfdb68a3a5aaa85e5d3048107be9e
ayu51591 <157950766+ayu51591@users.noreply.github.com>
2025-07-25 15:48:13 +0530

    Add files via upload

---
+++ magic.py
@@ -0,0 +1,39 @@
+import requests
+import json 
+import sys
+import wikipedia
+
+#url = "https://official-joke-api.appspot.com/jokes/programming/random"
+
+
+#response = requests.get("http://www.wikidata.org/entity/Q???")
+
+#response = requests.get("https://www.wikidata.org/wiki/Special:EntityData/Q42.json")
+topic = input("just something to  search -> ")
+
+data = wikipedia.summary(topic)
+
+try:
+    
+    data = wikipedia.summary(topic)
+    
+    # styling thing 
+    sentence = data.split('. ')
+    sentence = [sentence.strip() + '.' for sentence in sentence if sentence ]
+    
+    wiki_data = {
+        "topic" : topic,
+        "summary" : data
+        
+    } 
+    
+    with open("wiki_summary.json" , "w",encoding= "utf-8") as f:
+        json.dump(wiki_data , f, indent = 4, ensure_ascii= False)
+        
+    print("summary saved to wiki_data.json")
+    
+except wikipedia.exceptions.DisambiguationError as e:
+    print("too many request")    
+    print(e.options)
+except wikipedia.exceptions.PageError:
+    print("not found")    
